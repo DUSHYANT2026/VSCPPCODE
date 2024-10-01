c5f453dfc71ca75cd47c1a828e99b6cec78056de
Dushyant <dushyantpandit799@gmail.com>
2024-10-01 20:11:45 +0530

    mark all the changes,

--- app.cpp
+++ app.cpp
@@ -1,15 +1,175 @@
-#include<iostream>
+
+
+
+#include <bits/stdc++.h>
 using namespace std;
-long long fact(long long n){
-long long factorial=1;
-for(long long i=1;i<=n;i++){
-factorial*=i;}
-return factorial;}
-int main(){
-long long n;
-cin>>n;
-cout<<fact(n)<<endl;
-return 0;}
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *newNode(int data) {
+    struct Node *new_Node = new Node(data);
+
+    return new_Node;
+}
+
+Node *reverse(Node **r) {
+    Node *prev = NULL;
+    Node *cur = *r;
+    Node *nxt;
+    while (cur != NULL) {
+        nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    *r = prev;
+}
+
+void push(struct Node **head_ref, int new_data) {
+    
+    struct Node *new_Node = newNode(new_data);
+
+    new_Node->next = (*head_ref);
+
+    (*head_ref) = new_Node;
+}
+
+void freeList(struct Node *Node) {
+    struct Node *temp;
+    while (Node != NULL) {
+
+        temp = Node;
+        Node = Node->next;
+        free(temp);
+    }
+}
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};*/
+class solution {
+   private:
+    long long changetonumber(Node* list){
+        long long ans = 0;
+        while(list != NULL){
+            ans  = (ans*10 + (list->data)) % 1000000007;
+            list = list->next;
+        }
+        return ans;
+    }
+  public:
+    long long multiplyTwoLists(Node *first, Node *second) {
+        // code here
+        long long num1 = changetonumber(first);
+        long long num2 = changetonumber(second);
+        long long ans = 0;
+        
+        ans = (num1 * num2)%1000000007;
+         
+        // ADD TO NUMBERS
+        // long long carry = 0;
+        // long long ans = 0;
+        // long long place = 1;
+        
+        // while(num1 > 0 || num1 > 0 || carry > 0){
+        //     long long digit1 = num1%10;
+        //     long long digit2 = num2%10;
+            
+        //     long digitsum = digit1 + digit2 + carry;
+        //     long newdigit = digitsum % 10;
+            
+        //     ans = ans + newdigit * place;
+            
+        //     carry = digitsum/10;
+        //     num1 /= 10; num2 /= 10; 
+        //     place *= 10; 
+        // }
+
+        return ans;
+    }
+};
+void printList(struct Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+int main(void) {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        struct Node *first = NULL;
+        struct Node *second = NULL;
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        for (int i = 0; i < arr.size(); i++) {
+            push(&first, arr[i]);
+        }
+        vector<int> brr;
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number1;
+        while (ss2 >> number1) {
+            brr.push_back(number1);
+        }
+        for (int i = 0; i < brr.size(); i++) {
+            push(&second, brr[i]);
+        }
+        reverse(&first);
+        reverse(&second);
+        solution ob;
+        long long res = ob.multiplyTwoLists(first, second);
+        cout << res << endl;
+        freeList(first);
+        freeList(second);
+    }
+    return 0;
+}
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// long long fact(long long n){
+// long long factorial=1;
+// for(long long i=1;i<=n;i++){
+// factorial*=i;}
+// return factorial;}
+// int main(){
+// long long n;
+// cin>>n;
+// cout<<fact(n)<<endl;
+// return 0;}
 
 // #include<iostream>
 // using namespace std;
