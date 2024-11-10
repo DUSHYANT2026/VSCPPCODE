30610d5d2b10e85c1f22413916a3732ca411fed9
Dushyant <dushyantpandit799@gmail.com>
2024-11-10 19:49:38 +0530

    mark all the changes

--- dpcheck3.cpp
+++ dpcheck3.cpp
@@ -183,3 +183,170 @@ cout << "~" << "\n";
 }
     return 0; 
 }
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    vector<int> longestIncreasingSubsequence(int n, vector<int>& arr) {
+        vector<int> dp(n,1);
+        vector<int> check(n);
+        int lastindex = 0; int maxi = 0;
+        
+        for(int i=0; i<n; i++){
+            check[i] = i;
+            for(int j=0; j<i; j++){
+                if(arr[i] > arr[j] && dp[i] < 1 + dp[j]){
+                    dp[i] = 1 + dp[j];
+                    check[i] = j;
+                }
+            }
+            if(dp[i] > maxi){
+                maxi = dp[i];
+                lastindex = i;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(arr[lastindex]);
+        
+        while(check[lastindex] != lastindex){
+            lastindex = check[lastindex];
+            ans.push_back(arr[lastindex]);
+            
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        vector<int> res = obj.longestIncreasingSubsequence(N, arr);
+        for (auto x : res)
+            cout << x << " ";
+        cout << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+private:
+    int dpcheck(int n,int index,int preindex,int nums[],vector<vector<int>> &dp){
+        if(index == n) return 0;
+        if(dp[index][preindex+1] != -1) return dp[index][preindex+1];
+        
+        int notcount = 0 + dpcheck(n,index+1,preindex,nums,dp);
+        int count = 0;
+        if(preindex == -1 || nums[index] > nums[preindex]){
+            count = nums[index] + dpcheck(n,index+1,index,nums,dp);
+        }
+        int ans = max(count,notcount);
+        return dp[index][preindex+1] = ans;
+    }
+public:
+    //Max Sum Increasing Subsequence
+	int maxSumIS(int nums[], int n){  
+	    
+	    vector<vector<int>> dp(n,vector<int> (n+1,-1));
+	    
+	    return dpcheck(n,0,-1,nums,dp);
+	}  
+};
+int main() 
+{
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public: 
+    // Maximum sum of elements not part of LIS
+    int maxSumLis(int arr[], int n) {
+        int totalsum = 0;
+        
+        vector<int> dp(n,1);
+        vector<int> check(n);
+        int lastindex = 0; int maxi = 1;
+        
+        for(int i=0; i<n; i++){
+            totalsum += arr[i];
+            check[i] = i;
+            for(int j=0; j<i; j++){
+                if(arr[i] > arr[j] && dp[i] <= 1 + dp[j]){
+                    dp[i] = 1 + dp[j];
+                    check[i] = j;
+                }
+            }
+            if(dp[i] >= maxi){
+                maxi = dp[i];
+                lastindex = i;
+            }
+        }
+        int ans = 0;
+        ans += arr[lastindex];
+        
+        while(check[lastindex] != lastindex){
+            lastindex = check[lastindex];
+            ans += arr[lastindex];
+            
+        }
+        return totalsum - ans;
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int Arr[n];
+        for (int i = 0; i < n; i++) cin >> Arr[i];
+        Solution obj;
+        cout << obj.maxSumLis(Arr, n) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
