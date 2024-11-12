4d95f4a9decaac99343d28374e0f8b374a409a94
Dushyant <dushyantpandit799@gmail.com>
2024-11-12 19:37:23 +0530

    mark all the changes

--- dpcheckfinal.cpp
+++ dpcheckfinal.cpp
@@ -109,3 +109,171 @@
 // }
 //     return 0;
 // }
+
+
+
+
+
+
+
+
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& nums) {
+//         int n = nums.size();
+//         stack<int> s;
+
+//         int ans = 0;
+//         for(int i=0; i<=n; i++){
+//             while(!s.empty() && (i == n || nums[s.top()] >= nums[i])){
+//                 auto it = s.top();
+//                 s.pop();
+//                 int height = nums[it];
+//                 int width = 0;
+//                 if(s.empty()){
+//                     width = i;
+//                 }else{
+//                     width = i-s.top()-1;
+//                 }
+//                 ans = max(ans,height*width);
+//             }
+//             s.push(i);
+//         }
+//         return ans;
+//     }
+// };
+// int main(){
+//     int t;
+//     cin>>t;
+//     while(t--){
+//         int N;
+//         cin>>N;
+//         vector<int> arr;
+//         for(int i = 0;i < N;i++)
+//             cin>>arr[i];
+        
+//         Solution ob;
+//         cout<<ob.largestRectangleArea(arr)<<endl;
+    
+// cout << "~" << "\n";
+// }
+//     return 0;
+// }
+
+
+
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// private:
+//     int largestRectangleArea(vector<int>& nums) {
+//         int n = nums.size();
+//         stack<int> s;
+
+//         int ans = 0;
+//         for(int i=0; i<=n; i++){
+//             while(!s.empty() && (i == n || nums[s.top()] >= nums[i])){
+//                 auto it = s.top();
+//                 s.pop();
+//                 int height = nums[it];
+//                 int width = 0;
+//                 if(s.empty()){
+//                     width = i;
+//                 }else{
+//                     width = i-s.top()-1;
+//                 }
+//                 ans = max(ans,height*width);
+//             }
+//             s.push(i);
+//         }
+//         return ans;
+//     }
+// public:
+//     int maximalRectangle(vector<vector<int>>& matrix) {
+//         vector<int> nums(matrix[0].size(),0);
+
+//         int ans = INT_MIN;
+//         for(int i=0; i<matrix.size(); i++){
+//             for(int j=0; j<matrix[0].size(); j++){
+//                 if(matrix[i][j] == 1) nums[j]++;
+//                 else nums[j] = 0;
+//             }
+//             int maxarea = largestRectangleArea(nums);
+//             ans = max(ans,maxarea);
+//         }
+//         return ans;
+//     }
+// };
+// int main(){
+//     int n; int m; cin>>n>>m;
+//     vector<vector<int>> nums;
+//     for(int i=0; i<n; i++){
+//         vector<int> temp; int x;
+//         for(int j=0; j<m; j++){
+//             cin>>x;
+//             temp.push_back(x);
+//         }
+//         nums.push_back(temp);
+//     }
+//     Solution obj;
+//     cout<<obj.maximalRectangle(nums);
+//     return 0;
+// }
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size(); int m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j];
+        for(int i = 0; i < n; i++) dp[i][0] = matrix[i][0];
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] == 1){
+                    dp[i][j] = 1 + min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                }
+                else dp[i][j] = 0;
+            }
+        }
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                ans += dp[i][j];
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+    int n; int m; cin>>n>>m;
+    vector<vector<int>> nums;
+    for(int i=0; i<n; i++){
+        vector<int> temp; int x;
+        for(int j=0; j<m; j++){
+            cin>>x;
+            temp.push_back(x);
+        }
+        nums.push_back(temp);
+    }
+    Solution obj;
+    cout<<obj.countSquares(nums);
+    return 0;
+}
