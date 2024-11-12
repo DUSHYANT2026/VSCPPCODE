baef90700aafd3eab6b6267ece802164ce7f6cd8
Dushyant <dushyantpandit799@gmail.com>
2024-11-12 19:38:06 +0530

    done

--- dpcheckfinal.cpp
+++ dpcheckfinal.cpp
@@ -234,46 +234,46 @@
 
 
 
-#include<bits/stdc++.h>
-using namespace std;
-class Solution {
-public:
-    int countSquares(vector<vector<int>>& matrix) {
-        int n = matrix.size(); int m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     int countSquares(vector<vector<int>>& matrix) {
+//         int n = matrix.size(); int m = matrix[0].size();
+//         vector<vector<int>> dp(n, vector<int>(m, 0));
 
-        for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j];
-        for(int i = 0; i < n; i++) dp[i][0] = matrix[i][0];
+//         for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j];
+//         for(int i = 0; i < n; i++) dp[i][0] = matrix[i][0];
 
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < m; j++){
-                if(matrix[i][j] == 1){
-                    dp[i][j] = 1 + min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
-                }
-                else dp[i][j] = 0;
-            }
-        }
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                ans += dp[i][j];
-            }
-        }
-        return ans;
-    }
-};
-int main(){
-    int n; int m; cin>>n>>m;
-    vector<vector<int>> nums;
-    for(int i=0; i<n; i++){
-        vector<int> temp; int x;
-        for(int j=0; j<m; j++){
-            cin>>x;
-            temp.push_back(x);
-        }
-        nums.push_back(temp);
-    }
-    Solution obj;
-    cout<<obj.countSquares(nums);
-    return 0;
-}
+//         for(int i = 1; i < n; i++){
+//             for(int j = 1; j < m; j++){
+//                 if(matrix[i][j] == 1){
+//                     dp[i][j] = 1 + min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+//                 }
+//                 else dp[i][j] = 0;
+//             }
+//         }
+//         int ans = 0;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 ans += dp[i][j];
+//             }
+//         }
+//         return ans;
+//     }
+// };
+// int main(){
+//     int n; int m; cin>>n>>m;
+//     vector<vector<int>> nums;
+//     for(int i=0; i<n; i++){
+//         vector<int> temp; int x;
+//         for(int j=0; j<m; j++){
+//             cin>>x;
+//             temp.push_back(x);
+//         }
+//         nums.push_back(temp);
+//     }
+//     Solution obj;
+//     cout<<obj.countSquares(nums);
+//     return 0;
+// }
