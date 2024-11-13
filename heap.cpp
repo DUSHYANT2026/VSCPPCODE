d043c23a7400ce2c39f6fb4a515626e5ee4cbc2d
Dushyant <dushyantpandit799@gmail.com>
2024-11-13 12:14:24 +0530

    done

--- heap.cpp
+++ heap.cpp
@@ -635,3 +635,138 @@
 // cout<<a[i]<<" ";
 // return 0;}
 
+
+
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution{
+// private: 
+//     void heapify(int n, vector<int>& arr, int i) {
+//         int largest = i;
+//         int left = 2 * i + 1;
+//         int right = 2 * i + 2;
+        
+//         if (left < n && arr[left] > arr[largest])
+//             largest = left;
+//         if (right < n && arr[right] > arr[largest])
+//             largest = right;
+        
+//         if (largest != i) {
+//             swap(arr[i], arr[largest]);
+//             heapify(n, arr, largest);
+//         }
+//     }
+    
+//     void buildMaxHeap(vector<int>& arr, int n) {
+//         for (int i = n / 2 - 1; i >= 0; i--)
+//             heapify(n, arr, i);
+//     }
+
+// public:
+//     void convertMinToMaxHeap(vector<int> &arr, int N){
+//         buildMaxHeap(arr,N);
+//     }  
+// };
+// int main(){
+//     int t = 1;
+//     cin >> t;
+//     while(t--){
+//        int n; cin >> n;
+//        vector<int> vec(n);
+//        for(int i = 0;i<n;i++) cin >> vec[i];
+
+//         Solution obj;
+//         obj.convertMinToMaxHeap(vec,n);
+//         for(int i = 0;i<n;i++) cout << vec[i] << " ";
+//         cout << endl;
+// cout << "~" << "\n";
+// }
+//     return 0;
+// }
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution{
+//     private: 
+//     void heapify(int n, vector<int>& arr, int i) {
+//         int largest = i;
+//         int left = 2 * i + 1;
+//         int right = 2 * i + 2;
+        
+//         if (left < n && arr[left] > arr[largest])
+//             largest = left;
+//         if (right < n && arr[right] > arr[largest])
+//             largest = right;
+        
+//         if (largest != i) {
+//             swap(arr[i], arr[largest]);
+//             heapify(n, arr, largest);
+//         }
+//     }
+    
+//     void buildMaxHeap(vector<int>& arr, int n) {
+//         for (int i = n / 2 - 1; i >= 0; i--)
+//             heapify(n, arr, i);
+//     }
+    
+// public:
+//     vector<int> mergeHeaps(vector<int>& a, vector<int>& b, int n, int m) {
+//         vector<int> nums;
+//         nums.reserve(n + m);
+        
+//         for(int i : a) nums.push_back(i);
+//         for(int i : b) nums.push_back(i);
+        
+//         buildMaxHeap(nums, nums.size());
+//         return nums;
+//     }
+// };
+// bool isMerged(vector<int> &arr1, vector<int> &arr2, vector<int> &merged){
+//     if (arr1.size() + arr2.size() != merged.size()){
+//         return false;
+//     }
+//     arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+//     sort(arr1.begin(),arr1.end());
+//     vector<int> mergedCopy =merged;
+//     sort(mergedCopy.begin(),mergedCopy.end());
+//     if (arr1 != mergedCopy){
+//         return false;
+//     }
+//     for(int i = 1; i<merged.size(); i++){
+//         if(merged[i] > merged[(i-1)/2])
+//             return false;
+//     }
+//     return true;
+// }
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n, m, i;
+//         cin >> n >> m;
+//         vector<int> a(n,0),b(m,0);
+//         for (i = 0; i < n; i++) {
+//             cin >> a[i];
+//         }
+//         for (i = 0; i < m; i++) {
+//             cin >> b[i];
+//         }
+//         vector<int> merged, copyA = a, copyB = b;
+//         Solution obj;
+//         merged = obj.mergeHeaps(a, b, n, m);
+//         bool flag = isMerged(copyA, copyB, merged);
+//         if(flag == false) cout<<0<<endl;
+//         else cout<<1<<endl;
+    
+// cout << "~" << "\n";
+// }
+//     return 0;
+// }
