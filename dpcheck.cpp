e7b5cac86c97b57d176a8b1d81325aeb00af305c
Dushyant <dushyantpandit799@gmail.com>
2025-02-21 12:09:57 +0530

    mark all the changes

--- dpcheck.cpp
+++ dpcheck.cpp
@@ -1,21 +1,21 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int dpcheck(int n, vector<int> &nums){
-//     if(n <= 1) return n;
+#include<bits/stdc++.h>
+using namespace std;
+int dpcheck(int n, vector<int> &nums){
+    if(n <= 1) return n;
 
-//     if(nums[n] != -1) return nums[n];
-//     return nums[n] = dpcheck(n-1,nums) + dpcheck(n-2,nums);
-// }
-// int main(){
-//     int n;
-//     cin>>n;
-//     vector<int> nums(n+1,-1);
-//     cout<<dpcheck(n,nums);
-//     return 0;
+    if(nums[n] != -1) return nums[n];
+    return nums[n] = dpcheck(n-1,nums) + dpcheck(n-2,nums);
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n+1,-1);
+    cout<<dpcheck(n,nums);
+    return 0;
 
-//     // time = o(n) using vector
-//     // space = o(n) + o(n) (vector + recursion stack space)
-// }
+    // time = o(n) using vector
+    // space = o(n) + o(n) (vector + recursion stack space)
+}
 
 
 
