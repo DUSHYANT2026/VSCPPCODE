c4792699bc5149e17db42d2e9edba6cec0ea5e39
Dushyant <dushyantpandit799@gmail.com>
2025-04-05 01:08:53 +0530

    Done

--- dpcheck.cpp
+++ dpcheck.cpp
@@ -603,21 +603,21 @@ int main() {
 //     int uniquePaths(int m, int n) {                                                  // using tabulation
 //         vector<vector<int>> dp(n,vector<int>(m,-1));
         
-        // dp[0][0] = 1;
+//         dp[0][0] = 1;
 
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<m; j++){
-        //         if(i == 0 && j == 0){
-        //             dp[i][j] = 1;
-        //         }else {
-        //             int up = 0; int left = 0;
-        //             if(i > 0) up = dp[i-1][j];
-        //             if(j > 0) left = dp[i][j-1];
-        //             dp[i][j] = up+left;
-        //         }
-        //     }
-        // }
-        // return dp[n-1][m-1];
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(i == 0 && j == 0){
+//                     dp[i][j] = 1;
+//                 }else {
+//                     int up = 0; int left = 0;
+//                     if(i > 0) up = dp[i-1][j];
+//                     if(j > 0) left = dp[i][j-1];
+//                     dp[i][j] = up+left;
+//                 }
+//             }
+//         }
+//         return dp[n-1][m-1];
 //     }
 // };
 
@@ -975,6 +975,8 @@ int main() {
 //     cout<<obj.minFallingPathSum(nums);
 //     return 0;
 // }
+
+
 
 
 
